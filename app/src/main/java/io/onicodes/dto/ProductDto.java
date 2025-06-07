4cdabb7a3bb7d220cbab74723849f8535433ced1
abarolli <obarolli@gmail.com>
2025-06-07 12:09:34 -0700

    Add ProductCreationDto and ProductDto classes for product management

---
+++ app/src/main/java/io/onicodes/dto/ProductDto.java
@@ -0,0 +1,19 @@
+package io.onicodes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+public class ProductDto {
+    private Long id;
+    private String name;
+}
