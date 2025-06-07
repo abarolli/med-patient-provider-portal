86a1754c99a663258e364a5a2fd3b294c4a82adb
abarolli <obarolli@gmail.com>
2025-06-07 12:06:23 -0700

    Add Patient entity with fields for id, firstName, lastName, middleName, age, and isActive status

---
+++ app/src/main/java/io/onicodes/entity/Patient.java
@@ -0,0 +1,48 @@
+package io.onicodes.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "patients")
+public class Patient {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = true)
+    private String middleName;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+}
