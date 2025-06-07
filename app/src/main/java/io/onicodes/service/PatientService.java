8622fd76f6b6e756cc1eb8ec34a092d4eec2a4ed
abarolli <obarolli@gmail.com>
2025-06-07 14:26:25 -0700

    Add PatientService class for managing patient creation and updates

---
+++ app/src/main/java/io/onicodes/service/PatientService.java
@@ -0,0 +1,47 @@
+package io.onicodes.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.onicodes.dto.PatientCreationDto;
+import io.onicodes.dto.PatientDto;
+import io.onicodes.dto.PatientUpdateDto;
+import io.onicodes.entity.Patient;
+import io.onicodes.repository.PatientRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class PatientService {
+    
+    @Autowired
+    private PatientRepository patientRepository;
+
+    
+    @Transactional
+    public PatientDto create(PatientCreationDto creationRequest) {
+
+        Patient patient = new Patient();
+        patient.setFirstName(creationRequest.getFirstName());
+        patient.setLastName(creationRequest.getLastName());
+        patient.setMiddleName(creationRequest.getMiddleName());
+        patient.setAge(creationRequest.getAge());
+        return PatientDto.fromPatient(patientRepository.save(patient));
+    }
+
+    @Transactional
+    public PatientDto update(PatientUpdateDto updateRequest) {
+        long patientId = updateRequest.getId().get();
+        var patient = patientRepository
+            .findById(patientId)
+            .orElseThrow(() ->
+                new RecordNotFoundException(Patient.class, patientId));
+        
+        updateRequest.getFirstName().ifPresent(patient::setFirstName);
+        updateRequest.getLastName().ifPresent(patient::setLastName);
+        updateRequest.getMiddleName().ifPresent(patient::setMiddleName);
+        updateRequest.getAge().ifPresent(patient::setAge);
+        updateRequest.getIsActive().ifPresent(patient::setActive);
+
+        return PatientDto.fromPatient(patient);
+    }
+}
