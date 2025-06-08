cee67a10f06e26b55d8a788db1f6d0d245d8b74c
abarolli <obarolli@gmail.com>
2025-06-07 23:10:41 -0700

    Add findByLocations_Location_Id method to ProviderRepository for location-based provider retrieval

--- app/src/main/java/io/onicodes/repository/ProviderRepository.java
+++ app/src/main/java/io/onicodes/repository/ProviderRepository.java
@@ -1,5 +1,7 @@
 package io.onicodes.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import io.onicodes.entity.Provider;
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long>  {
     
+    public Page<Provider> findByLocations_Location_Id(Long locationId, Pageable pageable);
 }
