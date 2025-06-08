1f6a0a4a657c73217a577b6a7d4f157d11372b89
abarolli <obarolli@gmail.com>
2025-06-07 18:10:58 -0700

    Update ProviderLocationRepository to use ProviderLocationId as the identifier

--- app/src/main/java/io/onicodes/repository/ProviderLocationRepository.java
+++ app/src/main/java/io/onicodes/repository/ProviderLocationRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import io.onicodes.entity.ProviderLocation;
+import io.onicodes.entity.ProviderLocationId;
 
 @Repository
-public interface ProviderLocationRepository extends JpaRepository<ProviderLocation, Long> {
+public interface ProviderLocationRepository extends JpaRepository<ProviderLocation, ProviderLocationId> {
     
 }
