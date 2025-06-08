b6535c0da21a555f0fed1fc17d1811ddee852a77
abarolli <obarolli@gmail.com>
2025-06-08 00:53:06 -0700

    Add PatientController with getPatients method for paginated patient retrieval

---
+++ app/src/main/java/io/onicodes/controller/PagedResponse.java
@@ -0,0 +1,35 @@
+package io.onicodes.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+public class PagedResponse<T> {
+    
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public PagedResponse(Page<T> page) {
+        this.content = page.getContent();
+        this.pageNumber = page.getNumber() + 1; // Pages are 0-indexed
+        this.pageSize = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.last = page.isLast();
+    }
+}
---
+++ app/src/main/java/io/onicodes/controller/PatientController.java
@@ -0,0 +1,31 @@
+package io.onicodes.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.onicodes.dto.PatientDto;
+import io.onicodes.service.PatientService;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/patients")
+@Slf4j
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+    
+    @GetMapping
+    public ResponseEntity<PagedResponse<PatientDto>> getPatients(
+        @RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "10") int size) {
+
+        page = Math.max(0, page - 1); // maintains 1-based pagination
+        var patientsPage = patientService.getPatients(page, size);
+        return ResponseEntity.ok(new PagedResponse<>(patientsPage));
+    }
+}
