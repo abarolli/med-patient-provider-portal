13b40d8d189d305296c07a1d9f53fe78e4587b14
abarolli <obarolli@gmail.com>
2025-06-08 23:40:29 -0700

    Add AppointmentController for scheduling appointments

---
+++ app/src/main/java/io/onicodes/controller/AppointmentController.java
@@ -0,0 +1,26 @@
+package io.onicodes.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.onicodes.dto.AppointmentCreationDto;
+import io.onicodes.service.AppointmentService;
+
+@RestController
+@RequestMapping("/appointments")
+public class AppointmentController {
+    
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping("/schedule")
+    public ResponseEntity<String> scheduleAppointment(@ModelAttribute AppointmentCreationDto appointment) {
+        
+        var success = appointmentService.create(appointment);
+        return ResponseEntity.ok("Appointment scheduled for " + success.getAppointmentTime().toString());
+    }
+}
